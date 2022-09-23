663bc2c0cf017f6ed661d3f78e3c48928a7ec57b
Artur Senna <artursennass@gmail.com>
2022-09-23 15:34:47 -0300

    setando context

---
+++ src/context/RecipeProvider.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import RecipeContext from './RecipeContext';
+
+function RecipeProvider({ children }) {
+//   const contextValue = {
+
+  //   };
+
+  return (
+    <RecipeContext.Provider value={ contextValue }>
+      {children}
+    </RecipeContext.Provider>
+  );
+}
+
+RecipeProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default RecipeProvider;
