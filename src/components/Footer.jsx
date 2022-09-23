ab0991ea8132db9cef0fd42901233c2bccb19886
luis arthur <luisarthurlards03@gmail.com>
2022-09-23 17:40:17 -0300

    componente

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Footer = (props) => {
-
+function Footer(props) {
   const { history } = props;
 
-  return <footer data-testid="footer">
-    <button 
-    data-test-id="drinks-bottom-btn"
-    onClick={()=> history.push('/drinks') } >
-      <img src="src/images/drinkIcon.svg" alt="drink icon" />
-    </button>
-    <button 
-    data-test-id="meals-bottom-btn"
-    onClick={()=> history.push('/meals') }  >
-      <img src="src/images/mealIcon.svg" alt="meal icon" />
-    </button>
-  </footer>
+  return (
+    <footer data-testid="footer">
+      <button
+        type="button"
+        data-test-id="drinks-bottom-btn"
+        onClick={ () => history.push('/drinks') }
+      >
+        <img src="src/images/drinkIcon.svg" alt="drink icon" />
+      </button>
+      <button
+        type="button"
+        data-test-id="meals-bottom-btn"
+        onClick={ () => history.push('/meals') }
+      >
+        <img src="src/images/mealIcon.svg" alt="meal icon" />
+      </button>
+    </footer>
+  );
+}
+
+Footer.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
 };
+
+export default Footer;
