7ba62351786c03f2631922bfa759cfb80a0861de
Artur Senna <artursennass@gmail.com>
2022-09-23 19:10:41 -0300

    requisitos 8 e 9 feitos menos testes

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Route, Switch } from 'react-router-dom';
 import RecipeProvider from './context/RecipeProvider';
 import Login from './pages/Login';
 import Header from './components/Header';
+import Profile from './pages/Profile';
 
 function App() {
   return (
@@ -19,7 +20,7 @@ function App() {
         <Route exact path="/drinks/{id-da-receita}" component={ Login } />
         <Route exact path="/meals/{id-da-receita}/in-progress" component={ Login } />
         <Route exact path="/drinks/{id-da-receita}/in-progress" component={ Login } />
-        <Route exact path="/profile" component={ Login } />
+        <Route exact path="/profile" component={ Profile } />
         <Route exact path="/done-recipes" component={ Login } />
         <Route exact path="/favorite-recipes" component={ Login } />
       </Switch>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
@@ -6,13 +6,15 @@ import searchIcon from '../images/searchIcon.svg';
 //
 
 function Header() {
+  const [search, setSearch] = useState(false);
+  //   const [currentPage, setCurrentPage] = useState('');
+  const [conditional, setConditional] = useState(false);
+
   const history = useHistory();
-  console.log(history);
+  //   setCurrentPage(history.location.pathname);
   const currentPage = history.location.pathname;
   const pageTitle = currentPage.replace('/', '');
   const spacedPageTitle = pageTitle.split('-');
-
-  console.log(spacedPageTitle);
 
   const checkForHeaderSearch = currentPage === '/profile'
   || currentPage === '/done-recipes'
@@ -25,30 +27,50 @@ function Header() {
     return `${toCapital[0]} ${toCapital[1]}`;
   };
 
+  const processedTitle = spacedPageTitle.length === 2
+    ? putsBothInCapital()
+    : pageTitle.charAt(0).toUpperCase() + pageTitle.slice(1);
+
+  const handleProfileClick = () => {
+    history.push('/profile');
+    setConditional(true);
+    return conditional;
+  };
+
   return (
     <div>
       {!(currentPage === '/' || currentPage.includes('/', 2)) && (
         <header>
-          <h1 data-testid="page-title">
-            {
-              spacedPageTitle.length === 2
-                ? putsBothInCapital()
-                : pageTitle.charAt(0).toUpperCase() + pageTitle.slice(1)
-            }
-          </h1>
-          <img
-            src={ profileIcon }
-            alt="Icone de perfil"
-            data-testid="profile-top-btn"
-          />
+          <h1 data-testid="page-title">{processedTitle}</h1>
+          <button
+            type="button"
+            onClick={ handleProfileClick }
+          >
+            <img
+              src={ profileIcon }
+              alt="Icone de perfil"
+              data-testid="profile-top-btn"
+            />
+          </button>
           {
             !checkForHeaderSearch
-        && (<img
-          src={ searchIcon }
-          alt="Icone de pesquisa"
-          data-testid="search-top-btn"
-        />)
+        && (
+          <button
+            type="button"
+            onClick={ () => setSearch(!search) }
+          >
+            <img
+              src={ searchIcon }
+              alt="Icone de pesquisa"
+              data-testid="search-top-btn"
+            />
+          </button>
+
+        )
           }
+          {search && (
+            <input data-testid="search-input" placeholder="Aqui ficara o input" />
+          )}
         </header>
       )}
     </div>
