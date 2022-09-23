4e6028d91273c77f82edeb628c329881c86f9a09
Yury Stolarz Santana <yurysscareer@gmail.com>
2022-09-23 16:27:38 -0300

    requisito 2 completo

--- src/context/RecipeProvider.js
+++ src/context/RecipeProvider.js
@@ -3,9 +3,9 @@ import PropTypes from 'prop-types';
 import RecipeContext from './RecipeContext';
 
 function RecipeProvider({ children }) {
-  // const contextValue = {
+  const contextValue = {
 
-  //   };
+    };
 
   return (
     <RecipeContext.Provider value={ contextValue }>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,36 @@
 import React from 'react';
 
 function Login() {
+  // const emailIsValid = email.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{3})$/i);
   return (
-    <div>Login:</div>
+    <form>
+      <label htmlFor="email">
+        Email
+        <input
+          type="email"
+          name=""
+          id=""
+          data-testid="email-input"
+        />
+      </label>
+
+      <label htmlFor="email">
+        Senha
+        <input
+          type="text"
+          name=""
+          id=""
+          data-testid="password-input"
+        />
+      </label>
+
+      <button
+        type="submit"
+        data-testid="login-submit-btn"
+      >
+        Entrar
+      </button>
+    </form>
   );
 }
 
