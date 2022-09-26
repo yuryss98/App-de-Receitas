7961f35dbc6cb22349e2b1c3033e4ccf95205353
luis arthur <luisarthurlards03@gmail.com>
2022-09-26 17:12:08 -0300

    att nos testes

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -5,11 +5,20 @@ import App from '../App';
 import renderWithRouter from './helpers/renderWithRouter';
 
 describe('Testando componente Header', () => {
-  it('Se Header é renderizado na pagina meals', () => {
-    renderWithRouter(<App />, '/meals');
-    const headerTitle = screen.getByTestId('page-title');
-    expect(headerTitle).toBeInTheDocument();
-    expect(headerTitle.innerHTML).toBe('Meals');
+  it('Se Header é renderizado na pagina meals', async () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/meals');
+    expect(history.location.pathname).toBe('/meals');
+    const resultText = await screen.findByText(/Beef/i);
+    expect(resultText).toBeInTheDocument();
+  });
+
+  it('Se Header é renderizado drinks', async () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/drinks');
+    expect(history.location.pathname).toBe('/drinks');
+    const resultText = await screen.findByText(/Shake/i);
+    expect(resultText).toBeInTheDocument();
   });
 
   it('Se clicando no botão perfil muda a rota para /perfil', () => {
--- src/tests/RecipesTests.test.js
+++ src/tests/RecipesTests.test.js
@@ -1,22 +1,17 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
-import renderWithRouter from '../helpers/renderWithRouter';
+import renderWithRouter from './helpers/renderWithRouter';
 
-describe('Testando componente Header', () => {
-  it('Se Header é renderizado na pagina meals', async () => {
-    const { history } = renderWithRouter(<App />);
-    history.push('/meals');
-    expect(history.location.pathname).toBe('/meals');
-    const resultText = await screen.findByText(/Beef/i);
-    expect(resultText).toBeInTheDocument();
-  });
-
-  it('Se Header é renderizado drinks', async () => {
+describe('Testando pagina de receitas', () => {
+  it('verificar os filtros', async () => {
+    global.fetch = jest.fn().mockReturnValue(
+      { json: jest.fn().mockReturnValue([{}]) },
+    );
     const { history } = renderWithRouter(<App />);
     history.push('/drinks');
-    expect(history.location.pathname).toBe('/drinks');
-    const resultText = await screen.findByText(/Shake/i);
-    expect(resultText).toBeInTheDocument();
+    userEvent.click(screen.getByTestId('All-category-filter'));
+    expect(screen.getByTestId('All-category-filter')).toBeInTheDocument(); // dump test
   });
 });
