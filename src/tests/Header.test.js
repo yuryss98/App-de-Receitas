8960f6a8773ed6aa5d373b626f8f70217f9a77e9
luis arthur <luisarthurlards03@gmail.com>
2022-09-27 18:11:44 -0300

    +90 de cria

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from './helpers/renderWithRouter';
+import drinks from './helpers/drinks';
+import categoryMeals from './helpers/categoryMeals';
+import meals from './helpers/meals';
 
 window.alert = jest.fn();
 
@@ -67,5 +70,30 @@ describe('Testando componente Header', () => {
     expect(firstLetterRadioButton).toBeChecked();
     expect(window.alert).toHaveBeenCalledTimes(1);
     expect(window.alert).toHaveBeenCalledWith('Your search must have only 1 (one) character');
+  });
+  it('funciona com uma letra', async () => {
+    global.fetch = jest.fn((url) => {
+      let response = meals;
+      if (url === 'https://www.themealdb.com/api/json/v1/1/search.php?f=') response = meals;
+      if (url === 'https://www.thecocktail.com/api/json/v1/1/search.php?f=') response = drinks;
+      if (url === 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=') response = drinks;
+      if (url === 'https://www.themealdb.com/api/json/v1/1/list.php?c=list') response = categoryMeals;
+      if (url === `https://www.themealdb.com/api/json/v1/1/search.php?s=m`) response = { meals: null };
+      return Promise.resolve({
+        json: () => Promise.resolve(response),
+      });
+    });
+    renderWithRouter(<App />, '/meals');
+    const searchButton = screen.getByTestId(SEARC_BTN);
+    userEvent.click(searchButton);
+
+    const searchInput = screen.getByTestId('search-input');
+    const searchTypeButton = screen.getByTestId('exec-search-btn');
+    const firstLetterRadioButton = screen.getByTestId('first-letter-search-radio');
+    userEvent.type(searchInput, 'm');
+    userEvent.click(firstLetterRadioButton);
+    userEvent.click(searchTypeButton);
+    expect(firstLetterRadioButton).toBeChecked();
+    await waitFor(() => expect(global.fetch).toHaveBeenCalledWith("https://www.themealdb.com/api/json/v1/1/search.php?f=m"));
   });
 });
