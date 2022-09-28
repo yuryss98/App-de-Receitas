cf6ca3894d5407d31800a8638274ff119f4336e2
Artur Senna <artursennass@gmail.com>
2022-09-28 19:31:01 -0300

    requisitos 57 ate 62 feitos

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,9 +3,38 @@ import Footer from '../components/Footer';
 
 function Profile() {
   const history = useHistory();
+  const { email } = JSON.parse(localStorage.getItem('user'));
+
+  const handleLogout = () => {
+    localStorage.clear();
+    history.push('/');
+  };
   return (
     <>
-      <h1>Profile</h1>
+      <main>
+        <h2 data-testid="profile-email">{email}</h2>
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+          onClick={ () => history.push('/done-recipes') }
+        >
+          Done Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+          onClick={ () => history.push('/favorite-recipes') }
+        >
+          Favorite Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ handleLogout }
+        >
+          Logout
+        </button>
+      </main>
       <Footer history={ history } />
     </>
   );
